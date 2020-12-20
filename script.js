524b03c98d4200437103f5e314f69cd771ca7582
Juno Brown <amarbrown@gmail.com>
2020-12-19 20:53:41 -0500

    started on javascript, changed some ids in html

---
+++ script.js
@@ -0,0 +1,29 @@
+
+    //DOM Variables
+
+    //Javascript Variables
+var currentWeather = []
+    //Function Definitions
+
+    //Function Calls
+
+    //Event Listeners
+    $("citySearch").on("click", function() {
+    console.log("you searched");
+    
+    });
+
+
+//create the local storage for the api data to persist.
+
+//when you click the search button have it call the api
+
+//do the api
+
+//figure out what in the api object needs to display 
+
+//append the information to the right 
+
+//get the 5 day forcast api
+
+//append the 5 day forcast to the bottom 
