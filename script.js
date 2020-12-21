b40ae9313761c3d7605bcff26b81abbb44044816
Juno Brown <amarbrown@gmail.com>
2020-12-20 21:25:42 -0500

    button now calls api and get response back from server for current weather.  Set up vars for creating weather card.

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 
     //DOM Variables
-
-    //Javascript Variables
+    //API Call
+    //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+var apiKey = "&units=imperial&appid=95af86a9b5702eee43786687bca28ba8"
+// var city = $("#citySearch").val();
 var currentWeather = []
-    //Function Definitions
 
-    //Function Calls
+   
+
+  
 
     //Event Listeners
-    $("citySearch").on("click", function() {
-    console.log("you searched");
-    
+    $("#searchBtn").on("click", function(e) {
+        e.preventDefault();
+    city = $("#citySearch").val();
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q="+ city + apiKey
+    console.log(city);
+    console.log(apiKey);
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL, 
+        method:"GET"}).then(function(response){
+            console.log(response);
+        });
     });
+
+    function getCurrentWeather (response) {
+        var temp = (response.main.temp)
+        const card = $("<div>").addClass("card");
+    var cardBody = $("<div>").addClass("card-body");
+    var city = $("<h4>").addClass("card-title").text(response.name);
+    var cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+    var temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+    var humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+    var wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+    var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+    }
+    
+   
+    
 
 
 //create the local storage for the api data to persist.
